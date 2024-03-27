32b1f63401a871a3d3585656ae2e36e834fe101b
UmmmWilliam <134145680+UmmmWilliam@users.noreply.github.com>
2024-03-27 21:00:00 +0200

    Example of a forever loop
    Example of a forever loop using `While True: `

---
+++ inf.py
@@ -0,0 +1,52 @@
+
+import time
+
+a = 0
+b = float(input("Delay in s: "))
+c = input("Text of what you want to ask for (example: day 1 of asking for \"text here\"): ")
+
+while True:
+    print(f"Day {a + 1} of asking for {c}")
+    a = a + 1
+    time.sleep(b)
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
